24cd17b899172b27826551d8d5d61cbbd38a28d6
tamir_Shilony <tamirshilony@gmail.com>
2020-07-19 18:57:36 +0300

    spectrum

--- src/first_visualization.m
+++ src/first_visualization.m
@@ -13,10 +13,16 @@ function first_visualization(data,lable,lableNum,row,col)
         hold on
         C4 = plot(data(index_trails(i),:,2),'b');
         ylim([-20,20])
+        if(i==20)
+            xlabel('Nsample');
+        end
+        if(i==1)
+            ylabel('Mv');
+        end
    end
     hold on
     Leg = legend([C3,C4],{'C3','C4'});
     newUnits = 'normalized';
     set(Leg,'Position',[0.848091556210113 0.925793650793651 0.113690476190476 0.071031746031746],'Units', newUnits);
-    %set(Leg,'Location','northwest','Units', newUnits);
+    
 end
--- src/tamir.m
+++ src/tamir.m
@@ -9,8 +9,9 @@ dt = 1/Fs;                          % time step [sec]
 f = 0.5:0.1:40;
 windLen = 4;               % signal length in secs
 windStep = 2;              % window step in secs
-
-
+pwelchWindow = 2;           %window length in secs
+pwelchOverlap = 1;          %window overlap length in secs
+motorImageryRange = 240:1:768;
 %bends
 nFreqBands = 6;
 delta = 0.5:0.1:4.5;
@@ -26,4 +27,32 @@ left_index = find(P_C_S.attribute(3,:)==1);
 right_index = find(P_C_S.attribute(4,:)==1);
 first_visualization(P_C_S.data,P_C_S.attribute,3,5,4)
 first_visualization(P_C_S.data,P_C_S.attribute,4,5,4)
-%pwelch(P_C_S.data(left_index,:,1), 1, 1, f, fs);
+
+
+C3LeftPwelch = pwelch(P_C_S.data(left_index,(motorImageryRange),1),pwelchWindow,pwelchOverlap,f,Fs);
+C3RightPwelch = pwelch(P_C_S.data(right_index,(motorImageryRange),1),pwelchWindow,pwelchOverlap,f,Fs);
+C4LeftPwelch = pwelch(P_C_S.data(left_index,(motorImageryRange),2),pwelchWindow,pwelchOverlap,f,Fs);
+C4RightPwelch = pwelch(P_C_S.data(right_index,(motorImageryRange),2),pwelchWindow,pwelchOverlap,f,Fs);
+figure
+subplot(2,2,1)
+plot(f,mean(C3LeftPwelch,2))
+subplot(2,2,2)
+plot(f,mean(C4LeftPwelch,2))
+subplot(2,2,3)
+plot(f,mean(C3RightPwelch,2))
+subplot(2,2,4)
+plot(f,mean(C4RightPwelch,2))
+figure
+subplot(2,2,1)
+spectrogram(mean(C3LeftPwelch,2),5,pwelchOverlap,f,Fs)
+subplot(2,2,2)
+spectrogram(mean(C4LeftPwelch,2),5,pwelchOverlap,f,Fs)
+subplot(2,2,3)
+spectrogram(mean(C3RightPwelch,2),5,pwelchOverlap,f,Fs)
+subplot(2,2,4)
+spectrogram(mean(C4RightPwelch,2),5,pwelchOverlap,f,Fs)
+
+
+
+
+

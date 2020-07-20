99ecc135955941659b1785c49ccc7ca483c03e8c
tamir_Shilony <tamirshilony@gmail.com>
2020-07-20 15:15:07 +0300

    plot power spec and diff

--- src/tamir.m
+++ src/tamir.m
@@ -9,9 +9,9 @@ dt = 1/Fs;                          % time step [sec]
 f = 0.5:0.1:40;
 windLen = 4;               % signal length in secs
 windStep = 2;              % window step in secs
-pwelchWindow = 2;           %window length in secs
-pwelchOverlap = 1;          %window overlap length in secs
-motorImageryRange = 240:1:768;
+pwelchWindow = 2*Fs;           %window length in secs
+pwelchOverlap = 1*Fs;          %window overlap length in secs
+motorImageryRange = 2.5*Fs:6*Fs;
 %bends
 nFreqBands = 6;
 delta = 0.5:0.1:4.5;
@@ -25,33 +25,36 @@ waves = extractWavesIdx(delta, theta, lowAlpha, highAlpha, beta, gamma, f);
 
 left_index = find(P_C_S.attribute(3,:)==1);
 right_index = find(P_C_S.attribute(4,:)==1);
+
+left_C3 = P_C_S.data(left_index,:,1);
+right_C3 = P_C_S.data(right_index,:,1);
+left_C4 = P_C_S.data(left_index,:,2);
+right_C4 = P_C_S.data(right_index,:,2);
+
 first_visualization(P_C_S.data,P_C_S.attribute,3,5,4)
 first_visualization(P_C_S.data,P_C_S.attribute,4,5,4)
 
 
-C3LeftPwelch = pwelch(P_C_S.data(left_index,(motorImageryRange),1),pwelchWindow,pwelchOverlap,f,Fs);
-C3RightPwelch = pwelch(P_C_S.data(right_index,(motorImageryRange),1),pwelchWindow,pwelchOverlap,f,Fs);
-C4LeftPwelch = pwelch(P_C_S.data(left_index,(motorImageryRange),2),pwelchWindow,pwelchOverlap,f,Fs);
-C4RightPwelch = pwelch(P_C_S.data(right_index,(motorImageryRange),2),pwelchWindow,pwelchOverlap,f,Fs);
-figure
-subplot(2,2,1)
-plot(f,mean(C3LeftPwelch,2))
-subplot(2,2,2)
-plot(f,mean(C4LeftPwelch,2))
-subplot(2,2,3)
-plot(f,mean(C3RightPwelch,2))
-subplot(2,2,4)
-plot(f,mean(C4RightPwelch,2))
-figure
-subplot(2,2,1)
-spectrogram(mean(C3LeftPwelch,2),5,pwelchOverlap,f,Fs)
-subplot(2,2,2)
-spectrogram(mean(C4LeftPwelch,2),5,pwelchOverlap,f,Fs)
-subplot(2,2,3)
-spectrogram(mean(C3RightPwelch,2),5,pwelchOverlap,f,Fs)
-subplot(2,2,4)
-spectrogram(mean(C4RightPwelch,2),5,pwelchOverlap,f,Fs)
+C3LeftPwelch = pwelch(left_C3(:,motorImageryRange)',pwelchWindow,pwelchOverlap,f,Fs);
+C3RightPwelch = pwelch(right_C3(:,motorImageryRange)',pwelchWindow,pwelchOverlap,f,Fs);
+C4LeftPwelch = pwelch(left_C4(:,motorImageryRange)',pwelchWindow,pwelchOverlap,f,Fs);
+C4RightPwelch = pwelch(right_C4(:,motorImageryRange)',pwelchWindow,pwelchOverlap,f,Fs);
 
+plotPwelch(C3LeftPwelch,C3RightPwelch,C4LeftPwelch,C4RightPwelch,f)
+
+comparePowerSpec(C3LeftPwelch,C3RightPwelch,C4LeftPwelch,C4RightPwelch,f)
+
+
+
+
+
+
+
+
+% spectrogram(left_C3(i,:),pwelchWindow,pwelchOverlap,f,Fs,'yaxis')
+% spectrogram(right_C3(i,:),pwelchWindow,pwelchOverlap,f,Fs,'yaxis')
+% spectrogram(left_C4(i,:),pwelchWindow,pwelchOverlap,f,Fs,'yaxis')
+% spectrogram(right_C4(i,:),pwelchWindow,pwelchOverlap,f,Fs,'yaxis')
 
 
 

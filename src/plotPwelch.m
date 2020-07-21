7a5c10f2b213ac0ce4e87a297dfa486865404717
tamir_Shilony <tamirshilony@gmail.com>
2020-07-21 15:51:01 +0300

    fix title

--- src/plotPwelch.m
+++ src/plotPwelch.m
@@ -1,9 +1,9 @@
-function plotPwelch(condition,freq)
+function plotPwelch(condition,freq,generalTitle)
     figure
     for i = 1:length(condition)
         subplot(2,2,i)
         plot(freq,mean(condition{i},2))
-        %sgtitle('C3_left')
+        title(generalTitle{i});hold on
         if(i==1)
             ylabel('Power')
         end
@@ -11,4 +11,5 @@ function plotPwelch(condition,freq)
           xlabel('Frequency[Hz]')
         end
     end
+    hold off
 end
